36f3c1eadd644176ec8478c646aa9bbd9fb816b7
Fra <135766845+JungleKiosk@users.noreply.github.com>
2024-04-12 01:21:27 +0200

    import render_template

--- back_end/main.py
+++ back_end/main.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return 'Hello!'
+    return render_template('home/home.html')
+
 
 
 if __name__ == '__main__':
