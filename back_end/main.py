18392d3bcc7bddabe7469a3ab7a974295bcdcf58
Fra <135766845+JungleKiosk@users.noreply.github.com>
2024-04-12 02:14:55 +0200

    route /items def get_item

--- back_end/main.py
+++ back_end/main.py
@@ -7,7 +7,13 @@ app = Flask(__name__)
 def hello():
     return render_template('home/home.html')
 
+@app.route('/items')
+def get_items():
+    return render_template('home/cosmos/items.html')
 
+@app.route('/item')
+def get_item():
+    return render_template('home/cosmos/item.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
