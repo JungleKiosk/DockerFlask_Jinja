a5898ee7992fcdb2189fe790a3bebaed90fa2372
Fra <135766845+JungleKiosk@users.noreply.github.com>
2024-04-12 10:49:28 +0200

    route signup

--- back_end/main.py
+++ back_end/main.py
@@ -19,5 +19,10 @@ def get_item():
 def login():
     return render_template('home/user/login.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('home/user/signup.html')
+
+#-------------------------------------------------
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
